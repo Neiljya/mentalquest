eeb27cc30b9b712dd3a57b4a981e2cd5f801f390
neiljya <neil.j.wu@gmail.com>
2024-10-12 11:40:37 -0700

    add: server.js

--- mentalquest/backend/server.js
+++ mentalquest/backend/server.js
@@ -1,3 +1,33 @@
 const express = require('express')
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const router = require('./routes/router')
+const mongoose = require('mongoose')
+require(`dotenv/config`)
+
+const app = express()
+
+app.use(express.json())
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+    preflightContinue: false,
+    optionSuccessStatus: 204,
+    allowedHeaders: 'Content-Type, Authorization'
+}
+
+app.use(cors(corsOptions))
+app.use('/', router)
+
+const dbOptions = {useNewUrlParser: true, useUnifiedTopology: true}
+mongoose.connect(process.env.DB_URI, dbOptions)
+.then(() => console.log('Database Connected'))
+.catch(err => console.log(err))
+
+const port = process.env.PORT || 4000
+const server = app.listen(port, ()=>{
+    console.log(`Server is running on port ${port}`)
+})
