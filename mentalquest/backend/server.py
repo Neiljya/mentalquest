5f196771c90a3f2748929db1d6925b02bc5ede95
raymondwusuper <raymond.wu.super@gmail.com>
2024-10-13 08:39:31 -0700

    Update server.py

--- mentalquest/backend/server.py
+++ mentalquest/backend/server.py
@@ -85,9 +85,7 @@ class TherapistBot:
         return model_response
 
     def generate_tasks(self):
-        prompt = r"Generate outputs to questions by giving them a task list to improve their situation(s). These tasks will be used in a web app where the patient earns XP for completing tasks daily and levels up." + 
-                      r"Return the tasks in a JSON array with the format [{\"title\": String, \"xp_reward\": Int, \"completed\": Boolean (default false)}] and say nothing else." + 
-                      r"Generate an amount such that all of them can be completed in a day. Make sure the experience point rewards are multiples of 5. These tasks should be specific to the situation of the user."
+        prompt = r"Generate outputs to questions by giving them a task list to improve their situation(s). These tasks will be used in a web app where the patient earns XP for completing tasks daily and levels up. Return the tasks in a JSON array with the format [{\"title\": String, \"xp_reward\": Int, \"completed\": Boolean (default false)}] and say nothing else. Generate an amount such that all of them can be completed in a day. Make sure the experience point rewards are multiples of 5. These tasks should be specific to the situation of the user."
         return self.generate_content(prompt, update_history=False)
 
 ##########################################################
